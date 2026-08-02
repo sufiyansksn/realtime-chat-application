e52282166effc278d7f083f04f57778a60d4eab1
Shaik Sufiyan <sssk6317@gmail.com>
2026-08-02 16:28:48 +0530

    Day 6: Setup docker, Redis and  django channels and ASGI for websockets

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -10,7 +10,24 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter
+from channels.routing import URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+# application = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    "http" : django_asgi_app,
+
+    "websokets": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                
+            )
+        )
+    )
+})
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -148,3 +148,12 @@ SIMPLE_JWT = {
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": True,
 }
+
+CHANNEL_LAYERS = {
+    "default":{
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG":{
+            "hosts":[("127.0.0.1", 6379)]
+        },
+    },
+}
