0f306f1cf1beccdc5ffb57734c8924e41feeb12f
Shaik Sufiyan <sssk6317@gmail.com>
2026-09-22 17:57:00 +0530

    Implement persistent JWT authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,33 @@
 import Register from "./components/Register";
 import Login from "./components/Login";
 import ChatLayout from "./components/ChatLayout";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("access_token:");
+
+    if (!token) {
+        return;
+    }
+
+    fetch("http://127.0.0.1:8000/api/auth/me/", {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    })
+        .then((response) => {
+            console.log("ME response:", response.status);
+
+            if (response.ok){
+              setIsLoggedIn(true);
+            }
+        });
+
+  }, []);
 
   if (isLoggedIn){
     return <ChatLayout />
